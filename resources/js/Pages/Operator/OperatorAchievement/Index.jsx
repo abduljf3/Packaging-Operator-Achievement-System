17ff9123c9c795c2c62d8efe3fefede1b4982c9c
andhikaprima <dikapp23@gmail.com>
2023-03-27 11:26:55 +0700

    update index operator

--- resources/js/Pages/Operator/OperatorAchievement/Index.jsx
+++ resources/js/Pages/Operator/OperatorAchievement/Index.jsx
@@ -17,11 +17,18 @@ export default function index({ achievements, auth }) {
         await Inertia.delete(`/operator/operatorachievement/${id}`);
         setDeleting(false);
     };
+
+    function handleExport() {
+        exportToExcel(data, "users");
+    }
     return (
         <>
+            <div>
+                <button onClick={handleExport}>Export to Excel</button>
+            </div>
             <Nav roles={auth.roles} />
 
-            <ul>ADMIN achievements INDEX</ul>
+            <ul>OPERATOR achievements INDEX</ul>
             <Link href={route("operatorachievement.create")}>
                 Create New Product
             </Link>
@@ -57,90 +64,3 @@ export default function index({ achievements, auth }) {
         </>
     );
 }
-
-// import React from "react";
-// import ButtonRed from "@/Components/ButtonRed";
-// import ButtonGreen from "@/Components/ButtonGreen";
-// import TextInput from "@/Components/TextInput";
-// import InputError from "@/Components/InputError";
-// import InputLabel from "@/Components/InputLabel";
-// import Dropdown from "@/Components/Dropdown";
-// import OperatorLayout from "@/Layouts/OperatorLayout";
-// import Modal from "@/Components/Modal";
-
-// import { Link, Head, useForm } from "@inertiajs/react";
-
-// export default function achievement(props) {
-//     return (
-//         <>
-//             <Head title="Achievement" />
-//             <OperatorLayout>
-//                 <div className="py-5">
-//                     <div className="mmax-w-7xl mx-20 sm:px-6 lg:px-8 space-y-6">
-//                         <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-//                             <form className="flex justify-between">
-//                                 <div className=" mx-10 my-2">
-//                                     <InputLabel value="NPK" />
-//                                     <TextInput className="mb-5 block w-full " />
-//                                     <InputLabel value="Nama" />
-//                                     <TextInput className="mb-5 block w-full" />
-//                                     <InputLabel value="Date" />
-//                                     <TextInput
-//                                         type="date"
-//                                         className="mb-5 block w-full"
-//                                     />
-
-//                                     <div class="flex gap-4">
-//                                         <div class="mb-5">
-//                                             <InputLabel value="Shift" />
-//                                             <TextInput className="" />
-//                                         </div>
-//                                         <div class="mb-5">
-//                                             <InputLabel value="Group" />
-//                                             <TextInput className="" />
-//                                         </div>
-//                                     </div>
-//                                     <InputLabel value="Drawing Number" />
-//                                     <TextInput className="mb-5 block w-full" />
-//                                 </div>
-//                                 <div className="mx-10 my-2">
-//                                     <InputLabel value="Product Name" />
-//                                     <TextInput className="mb-5 block w-full" />
-//                                     <div class="flex gap-4">
-//                                         <div class="mb-5">
-//                                             <InputLabel value="Spring Lot No" />
-//                                             <TextInput className="" />
-//                                         </div>
-//                                         <div class="mb-5">
-//                                             <InputLabel value="Product Lot No" />
-//                                             <TextInput className="" />
-//                                         </div>
-//                                     </div>
-//                                     <div class="flex gap-4">
-//                                         <div class="mb-5">
-//                                             <InputLabel value="Total Lot" />
-//                                             <TextInput className="" />
-//                                         </div>
-//                                         <div class="mb-5">
-//                                             <InputLabel value="Qty(pcs)" />
-//                                             <TextInput className="" />
-//                                         </div>
-//                                     </div>
-
-//                                     <InputLabel value="Remaks" />
-//                                     <TextInput className="mb-5 block w-full" />
-//                                     <InputLabel value="Customer Id" />
-//                                     <TextInput className="mb-5 block" />
-//                                 </div>
-//                             </form>
-//                             <div className="flex justify-end gap-4 mx-10 my-2">
-//                                 <ButtonRed>Cancel</ButtonRed>
-//                                 <ButtonGreen>Save</ButtonGreen>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </OperatorLayout>
-//         </>
-//     );
-// }
