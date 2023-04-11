3f9a0f6bbd4bdace657f29c4254c706228964080
andhikaprima <dikapp23@gmail.com>
2023-04-11 16:06:50 +0700

    update slicing admin

--- resources/js/Pages/Admin/Employee/Create.jsx
+++ resources/js/Pages/Admin/Employee/Create.jsx
@@ -1,137 +1,226 @@
-import Nav from "@/Components/Nav";
-import { Link, useForm } from "@inertiajs/react";
+import React from "react";
+import ButtonGreen from "@/Components/ButtonGreen";
+import TextInput from "@/Components/TextInput";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import Authenticated from "@/Layouts/AuthenticatedLayout";
 import { useState } from "react";
+import { Link, Head, useForm } from "@inertiajs/react";
 
-export default function Create({auth}) {
-  const { data, setData, post, errors } = useForm({
- 
-   
-    fullname: "",
-    npk: "",
-    group: "",
-    status: "",
-    password: "",
-    roles: "",
- 
-
-   
-  });
-
-  const [submitting, setSubmitting] = useState(false);
-
-  const handleChange = (e) => {
-    const key = e.target.name;
-    const value = e.target.value;
-    setData((data) => ({ ...data, [key]: value }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setSubmitting(true);
-    await post("/admin/employee", {
-      data,
-      preserveScroll: true,
+export default function Create({ auth }) {
+    const { data, setData, post, errors } = useForm({
+        fullname: "",
+        npk: "",
+        group: "",
+        status: "",
+        password: "",
+        roles: "",
     });
-    setSubmitting(false);
-  };
 
-  return (
-    <>
-      <Nav roles={auth.roles} />
+    const [submitting, setSubmitting] = useState(false);
 
-      <form onSubmit={handleSubmit}>
-      <div>
-       <label htmlFor="fullname">fullname</label>
-       <input
-         type="text"
-         name="fullname"
-         value={data.fullname}
-         onChange={handleChange}
-         className={errors.fullname ? "border-red-500" : ""}  
-       />
-       {errors.fullname && (
-         <div className="text-red-500">{errors.fullname}</div>
-       )}
-     </div>
+    const handleChange = (e) => {
+        const key = e.target.name;
+        const value = e.target.value;
+        setData((data) => ({ ...data, [key]: value }));
+    };
 
-     <div>
-       <label htmlFor="npk">npk</label>
-       <input
-         type="text"
-         name="npk"
-         value={data.npk}
-         onChange={handleChange}
-         className={errors.npk ? "border-red-500" : ""}
-       />
-       {errors.npk && (
-         <div className="text-red-500">{errors.npk}</div>
-       )}
-     </div>
-     
-     
-     <div>
-          <label htmlFor="group">Group</label>
-          <input
-            type="text"
-            name="group"
-            value={data.group}
-            onChange={handleChange}
-            className={errors.group ? "border-red-500" : ""}
-          />
-          {errors.group && (
-            <div className="text-red-500">{errors.group}</div>
-          )}
-        </div>
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
+        await post("/admin/employee", {
+            data,
+            preserveScroll: true,
+        });
+        setSubmitting(false);
+    };
 
-        <div>
-          <label htmlFor="status">status</label>
-          <input
-            type="text"
-            name="status"
-            value={data.status}
-            onChange={handleChange}
-            className={errors.status ? "border-red-500" : ""}
-          />
-          {errors.status && (
-            <div className="text-red-500">{errors.status}</div>
-          )}
-        </div>
-       
-        <div>
-          <label htmlFor="password">User Id</label>
-          <input
-            type="password"
-            name="password"
-            value={data.password}
-            onChange={handleChange}
-            className={errors.password ? "border-red-500" : ""}
-          />
-          {errors.password && (
-            <div className="text-red-500">{errors.password}</div>
-          )}
-        </div>
+    return (
+        <>
+            <Head title="Add Operator" />
+            <Authenticated className="bg-white">
+                <div className="py-5 mx-20">
+                    <div className="mmax-w-7xl mx-20 sm:px-6 lg:px-8 space-y-6">
+                        <div className="p-4 mx-20 sm:p-8 bg-white shadow sm:rounded-lg">
+                            <div className="mb-6 font-extrabold">
+                                <h1>Create Operator</h1>
+                            </div>
+                            <form onSubmit={handleSubmit}>
+                                <div className="flex justify-center gap-20">
+                                    <div className=" mx-10 my-2">
+                                        <InputLabel value="NPK" />
+                                        <InputError message={errors.npk} />
+                                        <TextInput
+                                            className="mb-5 w-full block"
+                                            type="text"
+                                            name="npk"
+                                            value={data.npk}
+                                            onChange={handleChange}
+                                        />
 
-        <div>
-          <label htmlFor="roles">roles</label>
-          <input
-            type="text"
-            name="roles"
-            value={data.roles}
-            onChange={handleChange}
-            className={errors.roles ? "border-red-500" : ""}
-          />
-          {errors.roles && (
-            <div className="text-red-500">{errors.roles}</div>
-          )}
-        </div>
-        
-     
-        <div>
-          <button type="submit" disabled={submitting}>
-            {submitting ? "Creating..." : "Create"}
-          </button>
-        </div>
-      </form>
-    </>
-  );
+                                        <InputLabel value="Nama" />
+                                        <InputError message={errors.fullname} />
+                                        <TextInput
+                                            className="mb-5 w-full block"
+                                            type="text"
+                                            name="fullname"
+                                            value={data.fullname}
+                                            onChange={handleChange}
+                                        />
+
+                                        <InputLabel value="Group" />
+                                        <InputError message={errors.group} />
+                                        <TextInput
+                                            className="mb-5 block w-full "
+                                            type="text"
+                                            name="group"
+                                            value={data.group}
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    <div className="mx-10 my-2">
+                                        <InputLabel value="Status" />
+                                        <InputError message={errors.status} />
+                                        <TextInput
+                                            className="mb-5 block w-full "
+                                            type="text"
+                                            name="status"
+                                            value={data.status}
+                                            onChange={handleChange}
+                                        />
+
+                                        <InputLabel value="Password" />
+                                        <InputError message={errors.password} />
+                                        <TextInput
+                                            className="mb-5 block w-full "
+                                            type="password"
+                                            name="password"
+                                            value={data.password}
+                                            onChange={handleChange}
+                                        />
+
+                                        <InputLabel value="Roles" />
+                                        <InputError message={errors.roles} />
+                                        <TextInput
+                                            className="mb-5 block w-full "
+                                            type="text"
+                                            name="roles"
+                                            value={data.roles}
+                                            onChange={handleChange}
+                                        />
+                                        <div className="flex justify-center mt-6">
+                                            <ButtonGreen
+                                                type="submit"
+                                                disabled={submitting}
+                                                className=""
+                                            >
+                                                {submitting
+                                                    ? "Adding..."
+                                                    : "Add Operator"}
+                                            </ButtonGreen>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </Authenticated>
+        </>
+        // <>
+        //     <Nav roles={auth.roles} />
+
+        //     <form onSubmit={handleSubmit}>
+        //         <div>
+        //             <label htmlFor="fullname">fullname</label>
+        //             <input
+        //                 type="text"
+        //                 name="fullname"
+        //                 value={data.fullname}
+        //                 onChange={handleChange}
+        //                 className={errors.fullname ? "border-red-500" : ""}
+        //             />
+        //             {errors.fullname && (
+        //                 <div className="text-red-500">{errors.fullname}</div>
+        //             )}
+        //         </div>
+
+        //         <div>
+        //             <label htmlFor="npk">npk</label>
+        //             <input
+        //                 type="text"
+        //                 name="npk"
+        //                 value={data.npk}
+        //                 onChange={handleChange}
+        //                 className={errors.npk ? "border-red-500" : ""}
+        //             />
+        //             {errors.npk && (
+        //                 <div className="text-red-500">{errors.npk}</div>
+        //             )}
+        //         </div>
+
+        //         <div>
+        //             <label htmlFor="group">Group</label>
+        //             <input
+        //                 type="text"
+        //                 name="group"
+        //                 value={data.group}
+        //                 onChange={handleChange}
+        //                 className={errors.group ? "border-red-500" : ""}
+        //             />
+        //             {errors.group && (
+        //                 <div className="text-red-500">{errors.group}</div>
+        //             )}
+        //         </div>
+
+        //         <div>
+        //             <label htmlFor="status">status</label>
+        //             <input
+        //                 type="text"
+        //                 name="status"
+        //                 value={data.status}
+        //                 onChange={handleChange}
+        //                 className={errors.status ? "border-red-500" : ""}
+        //             />
+        //             {errors.status && (
+        //                 <div className="text-red-500">{errors.status}</div>
+        //             )}
+        //         </div>
+
+        //         <div>
+        //             <label htmlFor="password">User Id</label>
+        //             <input
+        //                 type="password"
+        //                 name="password"
+        //                 value={data.password}
+        //                 onChange={handleChange}
+        //                 className={errors.password ? "border-red-500" : ""}
+        //             />
+        //             {errors.password && (
+        //                 <div className="text-red-500">{errors.password}</div>
+        //             )}
+        //         </div>
+
+        //         <div>
+        //             <label htmlFor="roles">roles</label>
+        //             <input
+        //                 type="text"
+        //                 name="roles"
+        //                 value={data.roles}
+        //                 onChange={handleChange}
+        //                 className={errors.roles ? "border-red-500" : ""}
+        //             />
+        //             {errors.roles && (
+        //                 <div className="text-red-500">{errors.roles}</div>
+        //             )}
+        //         </div>
+
+        //         <div>
+        //             <button type="submit" disabled={submitting}>
+        //                 {submitting ? "Creating..." : "Create"}
+        //             </button>
+        //         </div>
+        //     </form>
+        // </>
+    );
 }
--- resources/js/Pages/Admin/Employee/Edit.jsx
+++ resources/js/Pages/Admin/Employee/Edit.jsx
@@ -1,118 +1,208 @@
-import React, { useState, useEffect } from 'react';
-import { Inertia } from '@inertiajs/inertia';
-import Nav from '@/Components/Nav';
+import React, { useState, useEffect } from "react";
+import { Inertia } from "@inertiajs/inertia";
+import ButtonGreen from "@/Components/ButtonGreen";
+import TextInput from "@/Components/TextInput";
+import InputLabel from "@/Components/InputLabel";
+import InputError from "@/Components/InputError";
+import Authenticated from "@/Layouts/AuthenticatedLayout";
+import { Link, Head, useForm } from "@inertiajs/react";
 
 export default function Edit({ users, auth }) {
-  const [state, setState] = useState({
-    id: users.id,
-    fullname: users.fullname,
-    npk: users.npk,
-    group: users.group,
-    status: users.status,
-    roles: users.roles,
-    password: users.password,
-   
-  });
+    const [state, setState] = useState({
+        id: users.id,
+        fullname: users.fullname,
+        npk: users.npk,
+        group: users.group,
+        status: users.status,
+        roles: users.roles,
+    });
 
-  useEffect(() => {
-    setState(users);
-  }, [users]);
+    useEffect(() => {
+        setState(users);
+    }, [users]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setState({ ...state, [name]: value });
-  };
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setState({ ...state, [name]: value });
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    Inertia.put(`/admin/employee/${state.id}`, state)
-      .then(() => {
-        // Redirect to the admin list
-        Inertia.visit('/admin/employee');
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        Inertia.put(`/admin/employee/${state.id}`, state)
+            .then(() => {
+                // Redirect to the admin list
+                Inertia.visit("/admin/employee");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
-  return (
-    <>
-      <Nav roles={auth.roles} />
+    return (
+        <>
+            <>
+                <Head title="Edit Operator" />
+                <Authenticated className="bg-white">
+                    <div className="py-5 mx-20">
+                        <div className="mmax-w-7xl mx-20 sm:px-6 lg:px-8 space-y-6">
+                            <div className="p-4 mx-20 sm:p-8 bg-white shadow sm:rounded-lg">
+                                <div className="mb-6 font-extrabold">
+                                    <h1>Edit Operator</h1>
+                                </div>
+                                <form onSubmit={handleSubmit}>
+                                    <div className="flex justify-center gap-20">
+                                        <div className=" mx-10 my-2">
+                                            <InputLabel value="NPK" />
 
-      <h1>Edit Product</h1>
+                                            <TextInput
+                                                className="mb-5 w-full block"
+                                                type="text"
+                                                name="npk"
+                                                value={state.npk}
+                                                onChange={handleChange}
+                                            />
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="fullname">fullname:</label>
-          <input
-            type="text"
-            id="fullname"
-            name="fullname"
-            value={state.fullname}
-            onChange={handleChange}
-          />
-        </div>
+                                            <InputLabel value="Nama" />
 
-        <div>
-          <label htmlFor="npk">npk:</label>
-          <input
-            type="text"
-            id="npk"
-            name="npk"
-            value={state.npk}
-            onChange={handleChange}
-          />
-        </div>
+                                            <TextInput
+                                                className="mb-5 w-full block"
+                                                type="text"
+                                                name="fullname"
+                                                value={state.fullname}
+                                                onChange={handleChange}
+                                            />
 
-        <div>
-          <label htmlFor="group">Group:</label>
-          <input
-            type="text"
-            id="group"
-            name="group"
-            value={state.group}
-            onChange={handleChange}
-          />
-        </div>
+                                            <InputLabel value="Group" />
 
-        <div>
-          <label htmlFor="status">status:</label>
-          <input
-            type="text"
-            id="status"
-            name="status"
-            value={state.status}
-            onChange={handleChange}
-          />
-        </div>
+                                            <TextInput
+                                                className="mb-5 block w-full "
+                                                type="text"
+                                                name="group"
+                                                value={state.group}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                        <div className="mx-10 my-2">
+                                            <InputLabel value="Status" />
 
-    
+                                            <TextInput
+                                                className="mb-5 block w-full "
+                                                type="text"
+                                                name="status"
+                                                value={state.status}
+                                                onChange={handleChange}
+                                            />
 
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={state.password}
-            onChange={handleChange}
-          />
-        </div>
+                                            <InputLabel value="Password" />
 
-        <div>
-          <label htmlFor="roles">Spring Lot:</label>
-          <input
-            type="text"
-            id="roles"
-            name="roles"
-            value={state.roles}
-            onChange={handleChange}
-          />
-        </div>
+                                            <TextInput
+                                                className="mb-5 block w-full "
+                                                type="password"
+                                                name="password"
+                                                value={state.password}
+                                                onChange={handleChange}
+                                            />
 
+                                            <InputLabel value="Roles" />
 
-        <button type="submit">submit</button>
-      </form>
-    </>
-  );
+                                            <TextInput
+                                                className="mb-5 block w-full "
+                                                type="text"
+                                                name="roles"
+                                                value={state.roles}
+                                                onChange={handleChange}
+                                            />
+                                            <div className="flex justify-center mt-6">
+                                                <ButtonGreen
+                                                    type="submit"
+                                                    className=""
+                                                >
+                                                    Update
+                                                </ButtonGreen>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </Authenticated>
+            </>
+        </>
+        // <>
+        //     <Nav roles={auth.roles} />
+
+        //     <h1>Edit Product</h1>
+
+        //     <form onSubmit={handleSubmit}>
+        //         <div>
+        //             <label htmlFor="fullname">fullname:</label>
+        //             <input
+        //                 type="text"
+        //                 id="fullname"
+        //                 name="fullname"
+        //                 value={state.fullname}
+        //                 onChange={handleChange}
+        //             />
+        //         </div>
+
+        //         <div>
+        //             <label htmlFor="npk">npk:</label>
+        //             <input
+        //                 type="text"
+        //                 id="npk"
+        //                 name="npk"
+        //                 value={state.npk}
+        //                 onChange={handleChange}
+        //             />
+        //         </div>
+
+        //         <div>
+        //             <label htmlFor="group">Group:</label>
+        //             <input
+        //                 type="text"
+        //                 id="group"
+        //                 name="group"
+        //                 value={state.group}
+        //                 onChange={handleChange}
+        //             />
+        //         </div>
+
+        //         <div>
+        //             <label htmlFor="status">status:</label>
+        //             <input
+        //                 type="text"
+        //                 id="status"
+        //                 name="status"
+        //                 value={state.status}
+        //                 onChange={handleChange}
+        //             />
+        //         </div>
+
+        //         <div>
+        //             <label htmlFor="password">Password:</label>
+        //             <input
+        //                 type="password"
+        //                 id="password"
+        //                 name="password"
+        //                 value={state.password}
+        //                 onChange={handleChange}
+        //             />
+        //         </div>
+
+        //         <div>
+        //             <label htmlFor="roles">Spring Lot:</label>
+        //             <input
+        //                 type="text"
+        //                 id="roles"
+        //                 name="roles"
+        //                 value={state.roles}
+        //                 onChange={handleChange}
+        //             />
+        //         </div>
+
+        //         <button type="submit">submit</button>
+        //     </form>
+        // </>
+    );
 }
