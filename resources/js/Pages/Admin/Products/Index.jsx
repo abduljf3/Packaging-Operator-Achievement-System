0700b6cc68d4d1c46cef4f0fabec12aa44ddff26
abduljf37 <abduljf37@gmail.com>
2023-06-19 14:14:58 +0700

    Ubah ID jadi NO ( All Table )

--- resources/js/Pages/Admin/Products/Index.jsx
+++ resources/js/Pages/Admin/Products/Index.jsx
@@ -68,7 +68,7 @@ export default function index({ products, auth }) {
     const columns = [
         {
             name: "No",
-            selector: (row) => row.id,
+            selector: (_, index) => index + 1,
             sortable: true,
         },
         {
