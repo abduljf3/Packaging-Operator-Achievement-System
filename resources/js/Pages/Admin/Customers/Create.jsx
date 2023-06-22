d5f26d81b529898989685574463763a85b64cdde
abduljf37 <abduljf37@gmail.com>
2023-06-22 11:23:30 +0700

    1 button customer

--- resources/js/Pages/Admin/Customers/Create.jsx
+++ resources/js/Pages/Admin/Customers/Create.jsx
@@ -6,6 +6,7 @@ import InputLabel from "@/Components/InputLabel";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
 import { useState } from "react";
 import Swal from "sweetalert2";
+import PrimaryButton from "@/Components/PrimaryButton";
 import { Head, Link, useForm } from "@inertiajs/react";
 
 export default function Create({ auth }) {
@@ -107,16 +108,17 @@ export default function Create({ auth }) {
                                          <div className="flex justify-center mt-6 gap-3 ml-9">
                                             <Link
                                                 href={route(
-                                                    "admin.employee.index"
+                                                    "admin.customers.index"
                                                 )}
-                                                className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-800"
+                                                
                                             >
-                                                Cancel
+                                                <PrimaryButton>cancel</PrimaryButton>
                                             </Link>
+                                            
                                             <ButtonGreen
                                                 type="submit"
                                                 disabled={submitting}
-                                                className="px-3"
+                                                className=""
                                             >
                                                 {submitting
                                                     ? "Adding..."
